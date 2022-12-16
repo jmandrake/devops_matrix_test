d4d012b8f28b9ff77ec51e48cbc720b2cfcbe77b
Jeffery Mandrake <mandrakebiz@gmail.com>
2022-12-16 08:27:58 +0000

    Added test coverage 100%

--- test_main.py
+++ test_main.py
@@ -3,8 +3,20 @@ Test goes here
 
 """
 
-from mylib.calculator import add
+from mylib.calculator import add, subtract, multiply, divide
 
 
 def test_add():
     assert add(1, 2) == 3
+
+
+def test_subtract():
+    assert subtract(5, 4) == 1
+
+
+def test_multiply():
+    assert multiply(4, 5) == 20
+
+
+def test_divide():
+    assert divide(9, 3) == 3
