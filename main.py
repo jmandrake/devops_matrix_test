5d0f4f852bf7b88e01c5dfacab04c3198f60a215
Jeffery Mandrake <mandrakebiz@gmail.com>
2022-12-16 08:16:24 +0000

    Testing error: this one should fail.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 Main cli or app entry point
 """
@@ -10,9 +12,20 @@ import click
 @click.argument("a", type=int)
 @click.argument("b", type=int)
 def add_cli(a, b):
+    """Add two numbers.
+
+    Args:\n
+        a (int)\n
+        b (int)\n
+    Example:\n
+        >>> python main.py add 3 5
+
+    """
     click.echo(add(a, b))
 
 
 if __name__ == "__main__":
     # pylint: disable=no-value-for-parameter
     add_cli()
+
+x=
